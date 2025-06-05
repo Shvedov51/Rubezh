7818af3b61fa4fdbd06feb9b6cda6a52d87e822f
Shvedov <shvedovmain@gmail.com>
2025-06-05 22:54:57 +0300

    Update rubezh.cpp

--- rubezh/rubezh.cpp
+++ rubezh/rubezh.cpp
@@ -1,6 +1,31 @@
 ﻿#include <iostream>
 
-int main()
+#include <GLFW/glfw3.h>
+#include <glad/glad.h> //https://www.reddit.com/r/cpp_questions/comments/ryr3fk/good_explanations_of_differences_between_glfw/?tl=ru
+
+void main()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(400, 400, "Window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, NULL);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return;
+    }
+
     std::cout << "Hello World!\n";
 }
