d82719d0700dad6339bc665d29d8607ea1279c50
Shvedov <shvedovmain@gmail.com>
2025-06-06 17:40:25 +0300

    imgui

--- rubezh/rubezh.cpp
+++ rubezh/rubezh.cpp
@@ -1,6 +1,16 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <glad/glad.h> //https://www.reddit.com/r/cpp_questions/comments/ryr3fk/good_explanations_of_differences_between_glfw/?tl=ru
 #include <GLFW/glfw3.h>
+
+#include"imgui.h"
+#include"imgui_impl_glfw.h"
+#include"imgui_impl_opengl3.h"
+
+void resizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 void main()
 {
@@ -9,24 +19,48 @@ void main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(400, 400, "Rubezh", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "Rubezh", NULL, NULL);
 
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, NULL);
+    glfwSetFramebufferSizeCallback(window, resizeCallback);
 
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    gladLoadGL();
 
     glClearColor(0.5, 0.5, 0.5, 1);
 
-    while (!glfwWindowShouldClose(window)) 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    while (true) 
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            break;
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
+        ImGui::Begin("Imgui window");
+        ImGui::Text("Sup");
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwDestroyWindow(window);
     glfwTerminate(); 
